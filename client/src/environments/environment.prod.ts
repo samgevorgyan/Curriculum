fb27ce4012f0618cc8bed80f257a16330957e583
Samvel Gevorgyan <samvelgevorgyan87@gmail.com>
2019-07-23 22:50:58 +0200

    betta stage 2

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  main_url : 'https://curriculumj.herokuapp.com'
+  // main_url : 'https://curriculumj.herokuapp.com'
+  main_url : 'https://samcv.net'
 };
